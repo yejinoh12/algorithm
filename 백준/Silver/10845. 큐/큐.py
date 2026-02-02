518fa90f7efdc3523fdefe47f9e0c3bd5f3da529
yejin oh <yjohh12@gmail.com>
2026-02-02 10:35:07 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+q = deque()
+
+# push, pop, size, empty, front, back
+
+for _ in range(N):
+    cmd = input().split()
+
+    if cmd[0] == "push":
+        q.append(int(cmd[1]))
+
+    elif cmd[0] == "pop":
+        print(q.popleft() if q else -1)
+
+    elif cmd[0] == "size":
+        print(len(q))
+
+    elif cmd[0] == "empty":
+        print(0 if q else 1)
+
+    elif cmd[0] == "front":
+        print(q[0] if q else -1)
+
+    elif cmd[0] == "back":
+        print(q[-1] if q else -1)
