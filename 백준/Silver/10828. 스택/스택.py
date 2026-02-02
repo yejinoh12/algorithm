04206f5c244485aa83e5d381f4c9f2ef3cb58eae
yejin oh <yjohh12@gmail.com>
2026-02-02 10:27:01 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    command = input().split()
+
+    if command[0] == "push":
+        stack.append(int(command[1]))
+
+    elif command[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif command[0] == "size":
+        print(len(stack))
+
+    elif command[0] == "empty":
+        print(0 if stack else 1)
+
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
