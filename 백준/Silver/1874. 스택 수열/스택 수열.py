820b401c56e7134b069e2bd86caf5f6f6a6f8248
yejin oh <yjohh12@gmail.com>
2026-02-04 09:40:24 +0900

    [Silver II] Title: 스택 수열, Time: 180 ms, Memory: 38876 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -7,38 +7,23 @@ target = [int(input()) for _ in range(N)]
 
 stack = []
 ops = []
-cur = 1 # 다음에 푸시 할 수 있는 가장 작은 숫자
+cur = 1
 
-for i in range(len(target)):
-    v = target[i]
-    # print(f"===============현재 꺼내야하는 숫자: {v}=====================")
-    # 스택이 비지 않고
-    if stack:
-        if stack[-1] > target[i]:
-            ops.clear()
-            break
-        elif stack[-1] == v:
-            stack.pop()
-            ops.append("-")
-            # print(">> 숫자 발견 팝 완료")
-            # print(">> 현재 스택", stack)
-            continue
-
+for v in target:
     while cur <= v:
         stack.append(cur)
         ops.append("+")
         cur += 1
-        # print(">>현재 스택", stack)
-        # print(">>현재 숫자:", cur)
 
-    stack.pop()
-    ops.append("-")
-    # print(f">>특정 숫자 {v} 까지 모두 넣고 팝 완료")
-    # print(">>현재 스택", stack)
-    # print(f"===============완료=====================")
+    if stack[-1] != v:
+        ops.clear()
+        break
+    else:
+        stack.pop()
+        ops.append("-")
 
 if ops:
-    for v in ops:
-        print(v)
+    for x in ops:
+        print(x)
 else:
     print("NO")
