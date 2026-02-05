598430a41654358819ba10f2ce09f544e0d02bf4
yejin oh <yjohh12@gmail.com>
2026-02-05 13:19:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 60 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,27 +1,11 @@
 from collections import deque
 
 K, N = map(int, input().split())
+q = deque(range(1, K + 1))
+ans = []
 
-q = deque()
-for i in range(1, K + 1):
-    q.append(i)
-
-i = 1
-arr = []
 while q:
-    if i % N == 0:
-        arr.append(q.popleft())
-    else:
-        q.append(q.popleft())
-    i += 1
+    q.rotate(-(N - 1))
+    ans.append(q.popleft())
 
-    if not q:
-        break
-
-print("<", end="")
-for i in range(len(arr)):
-    if i == len(arr) - 1:
-        print(arr[i], end='')
-    else:
-        print(arr[i], end=', ')
-print(">")
+print("<" + ", ".join(map(str, ans)) + ">")
